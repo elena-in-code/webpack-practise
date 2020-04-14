f03f22b6e0c39f9aece925ebde0da5eb0fba0409
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2020-04-14 11:52:52 +0200

    webpack config changes
    webpack config: change output file and add hashContent
    add hmtl-webpack-plugin
    clear dist to be generated automatically
    add @babel/preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
-module.exports = {
-  module:{
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node _modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react']
-          }
-        }
-      }
-    ]
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const javascriptRules = {
+  test: /\.js$/,
+  exclude: /node _modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-react', '@babel/preset-env']
+    }
   }
+}
+
+module.exports = {
+  output: {
+    filename: 'app.[contentHash].js'
+  },
+  module:{
+    rules: [javascriptRules]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 }
